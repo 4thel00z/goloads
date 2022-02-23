6fb77bd912c16c172978040b5ef9b9370b85b52b
4thel00z <4thel00z@gmail.com>
2022-02-23 14:57:07 +0100

    [master] Add some debug output
    Signed-off-by: 4thel00z <4thel00z@gmail.com>

--- entrypoint-git.sh
+++ entrypoint-git.sh
@@ -1,3 +1,7 @@
 #! /usr/bin/env sh
 
+echo "::debug::entrypoint-git.sh"
+echo "::debug::Args are $@"
+echo "::debug::Running git clone $1 /repo"
 git clone $1 /repo
+echo "::debug::Running ls on /repo: $(ls /repo)"
